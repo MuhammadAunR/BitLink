3ecf561f2fe64b82c2f075e97e53a30e38d3548c
M Aun Rasheed <greataun786@gmail.com>
2025-09-30 15:38:37 +0500

    Set Fonts

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Script from "next/script";
